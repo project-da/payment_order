15333735b8b019c5d7f642d714f3cdfd4e5a0810
project-da <90024133+project-da@users.noreply.github.com>
2021-10-06 12:36:38 +0530

    Update Manager_pre.php

--- Manager_pre.php
+++ Manager_pre.php
@@ -9,6 +9,32 @@ include_once('Manager_view1.php');
    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
     <body>
+       
+  <!-- Sidebar -->
+  <div class="w3-sidebar w3-bar-block w3-border-right" style="display:none" id="mySidebar">
+    <button onclick="w3_close()" class="w3-bar-item w3-large">Close &times;</button>
+    <a href="Manager_view.php" class="w3-bar-item w3-button">ALL PO'S</a>
+    <a href="Manager_pre.php" class="w3-bar-item w3-button">ALL PREVIEW </a>
+    <a href="logout.php" class="w3-bar-item w3-button">LOGOUT</a>
+  </div>
+  
+  <!-- Page Content -->
+  <div class="w3-teal">
+    <button class="w3-button w3-teal w3-xlarge" onclick="w3_open()">☰</button>
+   
+  </div>
+  
+  
+  
+  <script>
+  function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
+  </script>
         <div class="tab"> 
 <div class="container">
 <h2>   PAYMENT  ORDER       </h2></div>
