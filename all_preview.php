fbb5c67312d6e5dc9515db02473abd6a380dfc36
project-da <90024133+project-da@users.noreply.github.com>
2021-10-06 12:38:38 +0530

    Update all_preview.php

--- all_preview.php
+++ all_preview.php
@@ -9,9 +9,35 @@ include_once('create.php');
              <link rel="stylesheet" href="style.css"></link>
              <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            </head>
-        <body>
-     
+              <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    </head>
+<body>
+    
+     <!-- Sidebar -->
+  <div class="w3-sidebar w3-bar-block w3-border-right" style="display:none" id="mySidebar">
+    <button onclick="w3_close()" class="w3-bar-item w3-large">Close &times;</button>
+    <a href="Manager_view.php" class="w3-bar-item w3-button">ALL PO'S</a>
+    <a href="Manager_pre.php" class="w3-bar-item w3-button">ALL PREVIEW </a>
+    <a href="logout.php" class="w3-bar-item w3-button">LOGOUT</a>
+  </div>
+  
+  <!-- Page Content -->
+  <div class="w3-teal">
+    <button class="w3-button w3-teal w3-xlarge" onclick="w3_open()">☰</button>
+   
+  </div>
+  
+  
+  
+  <script>
+  function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
+  </script>
         <div class="pay" style=" margin-left: 11%;">    
       <table class="display" id="userTable" style="width:100%" >
                     <tr>   
